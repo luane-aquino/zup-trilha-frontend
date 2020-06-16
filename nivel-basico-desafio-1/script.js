197d5d8f6c9aad403f0574629fbc49a011ec23b5
Luane Aquino <luane.aquino.br@gmail.com>
2020-06-16 15:28:44 -0300

    add navbar

---
+++ nivel-basico-desafio-1/script.js
@@ -0,0 +1,16 @@
+const sidenav = document.getElementById("sidebar__container")
+const main = document.getElementById("main__container")
+const header = document.getElementById("header__container")
+const header_logo = document.getElementById('header__logo')
+
+function openSidenav() {
+  sidenav.classList.add("sidenav__open")
+}
+
+main.addEventListener("click", () => {
+  sidenav.classList.remove("sidenav__open")
+})
+
+header_logo.addEventListener("click", () => {
+  sidenav.classList.remove("sidenav__open")
+})
