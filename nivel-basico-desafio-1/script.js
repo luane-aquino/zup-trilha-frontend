493619b2bb8b5d52e85c83cf15048d120080f848
Luane Aquino <luane.aquino.br@gmail.com>
2020-06-23 14:05:36 -0300

    fill ui with user data

--- nivel-basico-desafio-1/script.js
+++ nivel-basico-desafio-1/script.js
@@ -1,17 +1,45 @@
 const nav = document.getElementById('nav__container')
-const section_search = document.getElementById('section__search')
-const header_btn = document.getElementById('header__menu-icon')
+const sectionSearch = document.getElementById('section__search')
+const headerBtn = document.getElementById('header__menu-icon')
+const userList = document.getElementById('main__list')
 
 // when click menu btn, show nav and hide searchbar
-header_btn.addEventListener('click', () => {
-  section_search.classList.toggle('section-close')
+headerBtn.addEventListener('click', () => {
+  sectionSearch.classList.toggle('section-close')
   nav.classList.toggle('nav-open')
 })
 
 // consume api
 async function getUsers() {
   let response = await fetch('https://jsonplaceholder.typicode.com/users')
-  let data = await response.json()
-  return data
+  let users = await response.json()
+  return users
 }
-getUsers().then(data => console.log(data))
+getUsers().then(users => fillUI(users))
+
+// fill ui with user data
+function generateHTML(users) {
+  let html = ''
+  users.forEach(user => {
+    html += `
+      <li>
+      <a class="main__list-item-container" href="./user.html">
+        <img src="./images/barbara-silva.jpg" alt="picture of Barbara Silva" class="main__list-item__picture">
+        <p class="main__list-item__name">${user.name}</p>
+        <p class="main__list-item__email">${user.email}</p>
+        <p class="main__list-item__phone">${user.phone}</p>
+        <p class="main__list-item__city">${user.address.city}</p>
+        <i class="fas fa-trash-alt" class="main__list-item__delete"></i>
+        <i class="far fa-list-alt" class="main__list-item__showall"></i>
+        <i class="fas fa-check-double" class="main__list-item__showcompleted"></i>
+      </a>
+    </li>
+  `
+  })
+  return html
+}
+
+function fillUI(users) {
+  const userHTML = generateHTML(users)
+  userList.innerHTML = userHTML
+}
