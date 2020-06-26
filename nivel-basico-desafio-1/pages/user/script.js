3c3612974fe44258a5e8c486480b656dbd54b08e
Luane Aquino <luane.aquino.br@gmail.com>
2020-06-26 14:40:02 -0300

    show user details on mouseover

--- nivel-basico-desafio-1/pages/user/script.js
+++ nivel-basico-desafio-1/pages/user/script.js
@@ -123,25 +123,3 @@ async function getUser(userId) {
   let user = await response.json()
   return user
 }
-
-// function generateHTMLOneUser(user) {
-//   const content = `
-//     <img src="${user.picture}" alt="${user.name}">
-//     <span class="main__title">Hi, My name is</span>
-//     <span class="main__info">${user.name}</span>
-//     <div class="main__icons">
-//       <i class="far fa-user"></i>
-//       <i class="far fa-envelope"></i>
-//       <i class="far fa-calendar-alt"></i>
-//       <i class="far fa-map"></i>
-//       <i class="fas fa-phone-volume"></i>
-//       <i class="fas fa-key"></i>
-//     </div>
-//   `
-//   return content
-// }
-
-// function fillUIOneUser(user) {
-//   const userHTML = generateHTMLOneUser(user)
-//   main.innerHTML = userHTML
-// }
